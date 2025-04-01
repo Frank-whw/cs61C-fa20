cc4806046008447f01eba79105beca9f529dfd2b
Frank-whw <2120320012@qq.com>
2025-04-01 18:24:15 +0800

    finish: Lab02 Exercise 2

--- fa20-lab-starter/lab02/lfsr.c
+++ fa20-lab-starter/lab02/lfsr.c
@@ -4,7 +4,14 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+unsigned get_bit(uint16_t reg, int n)
+{
+    return (reg >> n) & 1;
 }
 
+void lfsr_calculate(uint16_t *reg)
+{
+    /* YOUR CODE HERE */
+    unsigned MSB = get_bit(*reg, 0) ^ get_bit(*reg, 2) ^ get_bit(*reg, 3) ^ get_bit(*reg, 5);
+    *reg = (MSB << 15) | (*reg >> 1);
+}
